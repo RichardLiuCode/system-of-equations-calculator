292638e628e611e6faa8c37d89253de7b1aec00b
Richard Liu <richardadesigner@gmail.com>
2026-03-06 23:18:50 -0800

    update

--- script.js
+++ script.js
@@ -7,30 +7,69 @@ document.getElementById("convert").addEventListener("click", function () {
     const c2 = parseFloat(document.getElementById("c2").value);
     const y = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
     const x = (b2 * c1 - b1 * c2) / (a1 * b2 - a2 * b1);
-    document.getElementById("result").value = "The solution is x = " + x + " and " + "y = " + y;
+    const stepsTextString = `Step 1:
+         #--------#
+        ${a1}x + ${b1}y = ${c1}
+         #--------#
+        ${b1}y = ${c1} - ${a1}x
+         #--------#
+        y = ${c1 / b1} - ${a1}x ÷ ${b1}
+         #--------#
+         #--------#
+        Step 2:
+         #--------#
+        ${a2 != 1 ? a2 : ""}x + ${b2}y = ${c2}
+         #--------#
+        ${a2 != 1 ? a2 : ""}x + ${b2}(${c1 / b1} - ${a1}x/${b1}) = ${c2}
+         #--------#
+        ${a2 != 1 ? a2 : ""}x + ${b2 * (c1 / b1)} - ${b2}(${a1}x/${b1}) = ${c2}
+         #--------#
+        ${a2 != 1 ? a2 : ""}x - ${b2}(${a1}x/${b1}) = ${c2 - (b2 * (c1 / b1))}
+         #--------#
+        ${a2 != 1 ? a2 : ""}x - (${b2}(${a1}/${b1}))x = ${c2 - (b2 * (c1 / b1))}
+         #--------#
+        ${a2 != 1 ? a2 : ""}x - ${b2 * a1 / b1}x = ${c2 - (b2 * (c1 / b1))}
+         #--------#
+        ${a2 - b2 * a1 / b1}x = ${c2 - (b2 * (c1 / b1))}
+         #--------#
+        ${a2 - b2 * a1 / b1 == 1 ? `x = ${c2 - (b2 * (c1 / b1))} ` : ` ${a2 - b2 * a1 / b1}x/${a2 - b2 * a1 / b1} = ${c2 - (b2 * (c1 / b1))}/${a2 - b2 * a1 / b1}`}
+        ${a2 - b2 * a1 / b1 != 1 ? `#--------#x = ${(c2 - (b2 * (c1 / b1))) / (a2 - b2 * a1 / b1)}` : ""}
+         #--------#
+         #--------#
+        Step 3:
+         #--------#
+        ${a2}x + ${b2}y = ${c2}
+         #--------#
+        ${a2}(${(c2 - (b2 * (c1 / b1))) / (a2 - b2 * a1 / b1)}) + ${b2}y = ${c2}
+         #--------#
+         ${a2 * ((c2 - (b2 * (c1 / b1))) / (a2 - b2 * a1 / b1))} + ${b2}y = ${c2}
+         #--------#
+        ${b2}y = ${c2} - ${a2 * ((c2 - (b2 * (c1 / b1))) / (a2 - b2 * a1 / b1))}
+         #--------#
+        ${b2}y = ${c2 - (a2 * ((c2 - (b2 * (c1 / b1))) / (a2 - b2 * a1 / b1)))}
+         #--------#
+        ${b2 == 1 ? `y = ${c2 - (a2 * ((c2 - (b2 * (c1 / b1))) / (a2 - b2 * a1 / b1)))}` : `${b2}y/${b2} = ${c2 - (a2 * ((c2 - (b2 * (c1 / b1))) / (a2 - b2 * a1 / b1)))}/${b2}`}
+         #--------#
+        ${b2 != 1 ? `y = ${(c2 - (a2 * ((c2 - (b2 * (c1 / b1))) / (a2 - b2 * a1 / b1)))) / b2}` : ""}
+        `;
+    const stepsArray = stepsTextString.split("#--------#");
+    const beautifiedHTMLString = stepsArray.map(function (text) {
+        for (let i = 0; i < 7; i++) {
+            text = text.replace("- -", "+ ");
+            text = text.replace("+ -", "- ");
+            text = text.replace("\n", "");
+        }
+        return text;
+    }).join("<br>");
+
+    document.getElementById("result").innerHTML =
+        `<p style="border:none">${beautifiedHTMLString}</p>
+<p style="border:none">The solution is x = ${x} and y = ${y}</p>
+    `;
+
 });
 
 document.getElementById("inputValueCopyBtn").addEventListener("click", function () {
     document.getElementById("input").select();
     navigator.clipboard.writeText(document.getElementById("input").value);
 });
-document.getElementById("resultValueCopyBtn").addEventListener("click", function () {
-    document.getElementById("result").select();
-    navigator.clipboard.writeText(document.getElementById("result").value);
-});
-
-const ctx = document.getElementById("graph").getContext("2d");
-let maxWidth = document.getElementById("graph").width;
-let maxHeight = document.getElementById("graph").height;
-ctx.fillStyle = "white";;
-ctx.fillRect(0, 0, 1000, 1000);
-ctx.strokeStyle = "black";
-ctx.strokeWidth = "5";
-ctx.beginPath();
-for (let i = 0; i < 20; i++) {
-    ctx.moveTo(50, i * 50);
-    ctx.lineTo(maxWidth - 50, i * 50);
-    ctx.moveTo(i * 50, 50);
-    ctx.lineTo(i * 50, maxHeight - 50);
-}
-ctx.stroke();
